f59db64110578069b1ac139aeda0a335ca407ea9
abdielmontesg-glitch <abdielmontesg@gmail.com>
2026-02-23 10:56:24 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template,request, jsonify, make_response, session
 app = Flask(__name__)
 
-@app.route('/ciudades')
-def ciudades():
+@app.route('/usuarios')
+def usuarios():
     import mysql.connector
     mydb = mysql.connector.connect(
       host="46.28.42.226",
