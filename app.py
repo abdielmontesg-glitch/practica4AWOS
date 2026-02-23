7beecd139d0a649878fb8b80f12ceeda4a2f90ce
abdielmontesg-glitch <abdielmontesg@gmail.com>
2026-02-23 10:45:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template,request, jsonify, make_response, session
 app = Flask(__name__)
 
-@app.route('/productos')
-def productos():
+@app.route('/ciudades')
+def ciudades():
     import mysql.connector
     mydb = mysql.connector.connect(
-      host="localhost",
-      user="",
-      password="yourpassword",
-      database="mydatabase"
+      host="46.28.42.226",
+      user="u760464709_24005224_usr",
+      password="8PEd!gd5x+Sb",
+      database="u760464709_24005224_bd"
     )    
     mycursor = mydb.cursor()
     mycursor.execute("SELECT * FROM customers")
